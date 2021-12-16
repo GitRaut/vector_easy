d39e354723040c824d4303aef1c564adb7fba49b
Mango <92181249+GitRaut@users.noreply.github.com>
2021-12-16 12:52:17 +0300

    Update index_list.cpp

--- index_list.cpp
+++ index_list.cpp
@@ -3,10 +3,8 @@
 void itc_even_index_list(const vector <int> &mass, vector <int> &mass2){
     int count = mass.size();
     if(count != 0){
-        for(int i = 0; i < count; i++){
-            if(i % 2 == 1 or i == 0){
-                mass2.push_back(mass[i]);
-            }
+        for(int i = 0; i < count; i += 2){
+            mass2.push_back(mass[i]);
         }
     }
 }
