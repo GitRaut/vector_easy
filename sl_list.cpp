da3db61276cac5344ba73ed1cc25e6e81ed836f6
Mango <92181249+GitRaut@users.noreply.github.com>
2021-12-16 21:28:09 +0300

    Update sl_list.cpp

--- sl_list.cpp
+++ sl_list.cpp
@@ -4,7 +4,7 @@ int itc_sl_list(const vector <int> &mass){
     int count = mass.size();
     int col = 0;
     if(count != 0){
-        for(int i = 0; i< count; i++){
+        for(int i = 0; i < count - 1; i++){
             if(mass[i + 1] > mass[i]){
                 col++;
             }
